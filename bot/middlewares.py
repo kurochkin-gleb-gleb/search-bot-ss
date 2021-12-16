ee20b595efe22f1a5f11982ffb7a0022b38ab5b7
kurochkin-gleb-gleb <kurochkin.gleb.gleb@gmail.com>
2021-12-17 00:57:40 +0700

    final commit

--- bot/middlewares.py
+++ bot/middlewares.py
@@ -5,6 +5,6 @@ from aiogram.dispatcher.middlewares import BaseMiddleware
 class PrintMiddleware(BaseMiddleware):
 
     @staticmethod
-    async def on_pre_process_message(message: types.Message):
+    async def on_pre_process_message(message: types.Message, data: dict):
         print(message.from_user.username, message.from_user.full_name, message.text)
 
