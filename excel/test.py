a46961841bfae43e911691b294f4b8c5537ad194
kurochkin-gleb-gleb <kurochkin.gleb.gleb@gmail.com>
2021-12-18 23:57:18 +0700

    test Background Tasks with RQ

--- excel/test.py
+++ excel/test.py
@@ -3,7 +3,7 @@ import asyncio
 
 
 def test_queue(chat_id, text):
-    loop = asyncio.get_event_loop()
+    loop = asyncio.new_event_loop()
     loop.run_until_complete(asyncio.sleep(40))
     loop.run_until_complete(bot.send_message(chat_id, text))
     loop.close()
