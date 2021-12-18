8e2c7f37b946c17fd16a18a5a37eedea8f080573
kurochkin-gleb-gleb <kurochkin.gleb.gleb@gmail.com>
2021-12-19 01:23:04 +0700

    Background Tasks in Heroku with RQ

--- excel/process_excel_with_worker.py
+++ excel/process_excel_with_worker.py
@@ -42,7 +42,7 @@ async def work_with_excel(bot_message, file_name):
     salt = 1
     text = bot_message['text']
     while not os.path.exists(utils.get_path_to_excel_docs(file_name)):
-        print(utils.get_path_to_excel_docs(file_name), utils.get_path_to_excel_docs(''), os.listdir(utils.get_path_to_excel_docs('')))
+        print(utils.get_path_to_excel_docs(file_name), utils.get_path_to_excel_docs(''), os.listdir(utils.get_path_to_excel_docs('')[1:]))
         await asyncio.sleep(1)
     async for statistics in search_by_name(file_name):
         if statistics[0] == 'statistics':
