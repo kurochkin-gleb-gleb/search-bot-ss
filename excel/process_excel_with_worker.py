21f0ebc039e9d27990c7221e02ac2372c699a9e6
kurochkin-gleb-gleb <kurochkin.gleb.gleb@gmail.com>
2021-12-19 22:53:51 +0700

    Background Tasks in Heroku with RQ

--- excel/process_excel_with_worker.py
+++ excel/process_excel_with_worker.py
@@ -1,4 +1,6 @@
 import asyncio
+import aiohttp
+# import aiofiles
 import os
 
 from aiogram import types
@@ -8,6 +10,7 @@ from bot.bot import bot
 from bot.handlers import utils
 from bot.messages import bot_responses
 from excel.async_search import search_by_name
+from config import HIMERA_TOKEN_BOT
 
 
 def test_queue(chat_id, text):
@@ -40,11 +43,26 @@ async def end_process(bot_message, bot_message_, file_name, document):
 
 async def work_with_excel(bot_message, file_id):
     file = await bot.get_file(file_id)
+    file_name = file.file_unique_id + '.xlsx'
+    async with aiohttp.ClientSession() as session:
+        url = f'https://api.telegram.org/file/bot{HIMERA_TOKEN_BOT}/{file.file_id}'
+        print(url)
+        async with session.get(url) as response:
+            print('='*40)
+            print(response)
+            print(os.listdir('./excel'))
+            if not os.path.exists('.excel/documents'):
+                print('making')
+                os.mkdir('.excel/documents')
+            print(os.path.exists('.excel/documents'))
+            print(os.listdir('./excel'))
+            with open(f'.excel/documents/{file_name}', 'wb') as f:
+                f.write(await response.read())
     print(file)
     print(file.file_path)
     salt = 1
     text = bot_message['text']
-    async for statistics in search_by_name(utils.get_path_to_excel_docs(file_id + '.xlsx')):
+    async for statistics in search_by_name(utils.get_path_to_excel_docs(file_name)):
         if statistics[0] == 'statistics':
             new_text = bot_responses['searching']['statistics'].format(
                 number=statistics[1], all_number=statistics[2]
